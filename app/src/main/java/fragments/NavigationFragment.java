8c45e6b554abd62bbe5673200641d56bf5bbd49e
ahmadfarhan <ASOCS@users.noreply.github.com>
2017-11-02 22:24:15 +0500

    desigining nav bar -part 1

---
+++ app/src/main/java/fragments/NavigationFragment.java
@@ -0,0 +1,22 @@
+package fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.asocs.sprintmaster.R;
+
+/**
+ * Created by ASOCS on 11/2/2017.
+ */
+
+public class NavigationFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.bottom_menu_fragment,container,false);
+    }
+}
--- app/src/main/java/views/StartActivity.java
+++ app/src/main/java/views/StartActivity.java
@@ -1,9 +1,14 @@
 package views;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.asocs.sprintmaster.R;
+
+import fragments.NavigationFragment;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -11,5 +16,13 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN ,
+                 WindowManager.LayoutParams. FLAG_FULLSCREEN );
+        getSupportActionBar().hide();
+        NavigationFragment navigationFragment=new NavigationFragment();
+        FragmentTransaction transaction= getFragmentManager().beginTransaction();
+        transaction.add(R.id.navigation_view,navigationFragment);
+        transaction.commit();
     }
 }
