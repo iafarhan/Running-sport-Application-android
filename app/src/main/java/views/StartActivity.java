96c56b930480da8c1aa9a62b4f4e6dc0d462c2f2
ahmadfarhan <ASOCS@users.noreply.github.com>
2017-11-02 19:51:15 +0500

    Initial commit

---
+++ app/src/main/java/views/StartActivity.java
@@ -0,0 +1,15 @@
+package views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.asocs.sprintmaster.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
